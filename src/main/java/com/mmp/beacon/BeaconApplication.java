fc17d5a96118526eb6a4e2a10bad0242f384275a
sunyung0809 <sunyung0809@gmail.com>
2024-07-10 18:00:01 +0900

    feat: 초기 코드 구현

---
+++ src/main/java/com/mmp/beacon/BeaconApplication.java
@@ -0,0 +1,13 @@
+package com.mmp.beacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeaconApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmp/beacon/BeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmp.beacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
